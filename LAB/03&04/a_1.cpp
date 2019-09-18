17c0b3b2d0cc40f6c02710583133500567cd7833
Hyeonseok <kjbpoles@gmail.com>
2019-09-18 13:06:36 +0900

    LAB#3&4 upload

---
+++ LAB/03&04/a_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int square(int a);
+
+int main() {
+    int x;
+    x = 5;
+    cout << square(x) << endl;
+}
+
+int square(int a) {
+    return a * a;
+}
---
+++ LAB/03&04/b_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void print_DOB(int year = 1900, int month = 1, int day = 1);
+
+int main() {
+    int year, month, day;
+
+    print_DOB();
+    
+    cout << "year 입력 : ";
+    cin >> year;
+    cout << "month 입력 : ";
+    cin >> month;
+    cout << "day 입력 : ";
+    cin >> day;
+    
+    print_DOB(year, month, day);
+
+    return 0;
+}
+
+void print_DOB(int year, int month, int day) {
+    cout << "생년월일은 " << year << "년 " << month << "월 " << day <<"일입니다." << endl;
+}
